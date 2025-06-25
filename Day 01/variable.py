0d6d90d4a11938ebf46bf1d6ec553b09f9bea287
mdmanik74 <manikmhamud74@outlook.com>
2025-06-25 22:57:41 +0600

    variable

---
+++ Day 01/variable.py
@@ -0,0 +1,11 @@
+name="Afsana Akter"
+age= 24
+result = 3.21
+
+
+
+
+print("Our new student name is " +name)
+print(name + " She lives in dhaka ")
+print(" She is currently ",age," years old")
+print(" Has scored",result," in her seconday exam")

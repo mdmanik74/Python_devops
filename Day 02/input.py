d6b0204adccc3179eec47a311793e280149c115c
mdmanik74 <manikmhamud74@outlook.com>
2025-06-26 22:06:20 +0600

    input and operator

---
+++ Day 02/input.py
@@ -0,0 +1,11 @@
+#name=input("Enter your name: ")
+#print("Hello Mr. " + name)
+
+
+
+
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+print(f"Hello, {name}! You are {age} years old.")
+
---
+++ Day 02/operator.py
@@ -0,0 +1,14 @@
+a= 20
+b= 2
+#addition
+print("sum :", a+b)
+#subtraction
+print("subtraction : ", a-b)
+#multiplication
+print("Multiplication", a*b)
+#division
+print("Division :", a/b)
+#floor divison
+print("Floor Division", a%b)
+
+

16b88c85be6e24f2aefae545b89d65dd04da104b
mdmanik74 <manikmhamud74@outlook.com>
2025-06-28 22:30:52 +0600

    operator and libary

---
+++ Day 03/math funcation.py
@@ -0,0 +1,32 @@
+print(max(20,10))
+
+print(min(20,10))
+
+
+a = 33
+b = 200
+if b > a:
+  print("b is greater than a")
+  
+number= int(input('Enter a number'))
+
+if number >0:
+ print("Positive number")
+else:
+    print("Not a Positive number")
+print("This is statement always executes")
+  a= 5
+b = 3
+result= a+b
+print(result)
+
+
+d=5
+k=5
+print(d !=k)
+
+x=5
+x-=3
+print(x)
+
+
